29560081e99115b9f155eee306779904744f9406
shreayan98c <shreayan98c@gmail.com>
2023-04-25 01:47:59 -0400

    Siamese Network trainer

--- PPI_Pred/utils.py
+++ PPI_Pred/utils.py
@@ -134,3 +134,66 @@ def train_simple_linear_model(
                 correct += (predicted == target).sum().item()
 
         log.info(f"Epoch {epoch} accuracy: {correct / total:.4f}")
+
+
+def train_siamese_model(
+        model: nn.Module,
+        train_loader,
+        test_loader,
+        epochs: int,
+        lr: float,
+        logging_interval: int = 100,
+):
+    """
+    Train a simple linear model.
+    :param model: model to train
+    :param train_loader: train loader data
+    :param test_loader: test loader data
+    :param epochs: number of epochs
+    :param lr: learning rate
+    :param logging_interval: number of batches between logging
+    :return:
+    """
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    avg_loss = 0.0
+    train_acc_store = []
+    val_acc_store = []
+
+    for epoch in range(epochs):
+        model.train()
+        for batch_idx, batch in track(
+                enumerate(train_loader), total=len(train_loader), description=f"Train epoch {epoch}"
+        ):
+            seq1, seq2, target = batch['seq1_input_ids'].float(), batch['seq1_input_ids'].float(), batch['label']
+            target = target.unsqueeze(1).float()
+            optimizer.zero_grad()
+            output = model(seq1, seq2)
+            loss = criterion(output, target)
+            avg_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
+            if batch_idx % logging_interval == 0:
+                log.info(f"Epoch {epoch} batch {batch_idx} loss: {avg_loss / logging_interval:.4f}")
+                avg_loss = 0.0
+
+        if epoch % 5 == 0:
+            torch.save(model, "model.pt")
+
+        model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for batch_idx, batch in track(
+                    enumerate(test_loader), total=len(test_loader), description=f"Test epoch {epoch}"
+            ):
+                seq1, seq2, target = batch['seq1_input_ids'].float(), batch['seq1_input_ids'].float(), batch['label']
+                target = target.unsqueeze(1).float()
+                output = model(seq1, seq2)
+                predicted = torch.round(output.data)
+                total += target.size(0)
+                correct += (predicted == target).sum().item()
+
+        log.info(f"Epoch {epoch} accuracy: {correct / total:.4f}")
