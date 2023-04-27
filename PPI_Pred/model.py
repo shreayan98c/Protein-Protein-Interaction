46b6ac4739139b27527585c5b49f51bb128cf3d7
shreayan98c <shreayan98c@gmail.com>
2023-04-26 22:12:33 -0400

    Model architecture for Siamese network

--- PPI_Pred/model.py
+++ PPI_Pred/model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class MyModel(nn.Module):
@@ -68,48 +69,87 @@ class SimpleLinearModel(nn.Module):
 
 
 class SiameseNetwork(nn.Module):
-    def __init__(self, d: int = 10000):
+    def __init__(self, d: int = 1):
         super(SiameseNetwork, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=d, out_channels=32, kernel_size=3)
-        self.batchnorm1 = nn.BatchNorm1d(32)
+
+        # blocks of convolutional layers followed by batch normalization, relu, and max pooling
+        self.conv1 = nn.Conv1d(in_channels=d, out_channels=16, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm1d(num_features=16)
         self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool1d(kernel_size=2)
-        self.conv2 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3)
-        self.batchnorm2 = nn.BatchNorm1d(64)
+        self.pool1 = nn.MaxPool1d(kernel_size=2, stride=2)
+
+        self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm1d(num_features=32)
         self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool1d(kernel_size=2)
-        self.conv3 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3)
-        self.batchnorm3 = nn.BatchNorm1d(128)
+        self.pool2 = nn.MaxPool1d(kernel_size=2, stride=2)
+
+        self.conv3 = nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm1d(num_features=64)
         self.relu3 = nn.ReLU()
-        self.pool3 = nn.MaxPool1d(kernel_size=2)
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(128 * 9, 512)
+        self.pool3 = nn.MaxPool1d(kernel_size=2, stride=2)
+
+        # define fully connected layers
+        self.fc1 = nn.Linear(in_features=64 * 625, out_features=128)
+        self.bn4 = nn.BatchNorm1d(num_features=128)
         self.relu4 = nn.ReLU()
-        self.fc2 = nn.Linear(512, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.dropout1 = nn.Dropout(p=0.5)
+
+        self.fc2 = nn.Linear(in_features=128, out_features=64)
+        self.bn5 = nn.BatchNorm1d(num_features=64)
+        self.relu5 = nn.ReLU()
+        self.dropout2 = nn.Dropout(p=0.5)
+
+        self.fc3 = nn.Linear(in_features=64, out_features=1)
 
     def forward_once(self, x):
+        # convolutional layers
         x = self.conv1(x)
-        x = self.batchnorm1(x)
+        x = self.bn1(x)
         x = self.relu1(x)
         x = self.pool1(x)
+
         x = self.conv2(x)
-        x = self.batchnorm2(x)
+        x = self.bn2(x)
         x = self.relu2(x)
         x = self.pool2(x)
+
         x = self.conv3(x)
-        x = self.batchnorm3(x)
+        x = self.bn3(x)
         x = self.relu3(x)
         x = self.pool3(x)
-        x = self.flatten(x)
+
+        # flatten for fully connected layers
+        x = x.view(x.size(0), -1)
+
+        # fully connected layers
         x = self.fc1(x)
+        x = self.bn4(x)
         x = self.relu4(x)
+        x = self.dropout1(x)
+
+        x = self.fc2(x)
+        x = self.bn5(x)
+        x = self.relu5(x)
+        x = self.dropout2(x)
+
         return x
 
     def forward(self, input1, input2):
+
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
-        concatenated = torch.cat((output1, output2), 1)
-        x = self.fc2(concatenated)
-        x = self.sigmoid(x)
-        return x
+
+        # calculate Euclidean distance between outputs
+        distance = F.pairwise_distance(output1, output2)
+        distance = distance.view(-1, 1)
+
+        print(distance.shape)
+
+        # pass through final fully connected layer
+        # out = self.fc3(distance)
+
+        out = F.sigmoid(self.fc3(distance))
+
+        print(out.shape)
+
+        return out
