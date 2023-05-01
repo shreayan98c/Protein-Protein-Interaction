6236f72537b0f2c9404ecefdffad653be7926d87
shreayan98c <shreayan98c@gmail.com>
2023-05-01 19:16:00 -0400

    Training for Siamese Net

--- PPI_Pred/model.py
+++ PPI_Pred/model.py
@@ -1,6 +1,4 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+from PPI_Pred.losses import *
 
 
 class MyModel(nn.Module):
@@ -139,17 +137,74 @@ class SiameseNetwork(nn.Module):
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
 
+        return output1, output2
+
         # calculate Euclidean distance between outputs
-        distance = F.pairwise_distance(output1, output2)
-        distance = distance.view(-1, 1)
+        # distance = F.pairwise_distance(output1, output2)
+        # distance = distance.view(-1, 1)
+        #
+        # print(distance.shape)
+        #
+        # # pass through final fully connected layer
+        # # out = self.fc3(distance)
+        #
+        # out = F.sigmoid(self.fc3(distance))
+        #
+        # print(out.shape)
 
-        print(distance.shape)
+        # return out
 
-        # pass through final fully connected layer
-        # out = self.fc3(distance)
 
-        out = F.sigmoid(self.fc3(distance))
+# create a siamese network
+# References:
+# https://builtin.com/machine-learning/siamese-network
+# https://www.projectpro.io/article/siamese-neural-networks/718
+# https://www.kaggle.com/parthdhameliya77/siamese-network-approach-82-acc
+class SigNetSiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SigNetSiameseNetwork, self).__init__()
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(1, 96, kernel_size=11, stride=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
 
-        print(out.shape)
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5, alpha=0.0001, beta=0.75, k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
 
-        return out
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout2d(p=0.3),
+        )
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(30976, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=0.5),
+
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(128, 2))
+
+    def forward_once(self, x):
+        # Forward pass
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.forward_once(input1)
+        # forward pass of input 2
+        output2 = self.forward_once(input2)
+        return output1, output2
