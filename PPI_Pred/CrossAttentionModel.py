beb5ebac79997995722740e7d0b2b69d6a145908
jacky <jc200108@gmail.com>
2023-05-01 19:02:13 -0400

    add cross_attention starting point

--- PPI_Pred/CrossAttentionModel.py
+++ PPI_Pred/CrossAttentionModel.py
@@ -2,26 +2,23 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class CrossAttentionModel(nn.Module):
-    def __init__(self, d: int 1):
+class CrossAttentionBlock(nn.Module):
+    """
+    Cross Attention Block with res connections, batch norm and a feed forward nn as defined in Attention is All You Need"
+    """
+    def __init__(self,embed_dim, num_heads, dropout=0.0, bias=True, add_bias_kv=False, add_zero_attn=False, kdim=None,\
+        vdim=None, batch_first=False, device=None, dtype=None):
+        """ 
+        Instantiation takes same args as torch.nn.MultiheadedAttention
+        """
         super().__init__()
 
         # Block to pass input 1 through before passing to cross attention layer
-        block_input1 = nn.Sequential(
-            nn.Conv1d(in_channels=d, out_channels=16, kernel_size=3, padding=1),
-            nn.BatchNorm1d(num_features=16),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2),
-            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
-            nn.BatchNorm1d(num_features=32),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2),
-            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
-            nn.BatchNorm1d(num_features=64),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2, stride=2),
-            nn.Linear(in_features=64 * 625, out_features=128)
-        )
+        self.query = nn.Linear()
+        self.key = nn.Linear(in_features, out_features)
+        self.value = nn.Linear(in_features, out_features)
+        self.MultiheadedAttention = nn.MultiheadAttention(embed_dim, num_heads)
+        
 
     def forward(self, input1, input2):
-        raise NotImplementedError
+        
