6f32dec4136c8e0c26f8cc75ed9eba34108f56fc
jacky <jc200108@gmail.com>
2023-05-01 19:20:59 -0400

    changed dimensions

--- PPI_Pred/CrossAttentionModel.py
+++ PPI_Pred/CrossAttentionModel.py
@@ -6,7 +6,7 @@ class CrossAttentionBlock(nn.Module):
     """
     Cross Attention Block with res connections, batch norm and a feed forward nn as defined in Attention is All You Need"
     """
-    def __init__(self,embed_dim, num_heads, dropout=0.0, bias=True, add_bias_kv=False, add_zero_attn=False, kdim=None,\
+    def __init__(self,embed_dim, kqv_dims, num_heads, dropout=0.0, bias=True, add_bias_kv=False, add_zero_attn=False, kdim=None,\
         vdim=None, batch_first=False, device=None, dtype=None):
         """ 
         Instantiation takes same args as torch.nn.MultiheadedAttention
@@ -14,11 +14,20 @@ class CrossAttentionBlock(nn.Module):
         super().__init__()
 
         # Block to pass input 1 through before passing to cross attention layer
-        self.query = nn.Linear()
-        self.key = nn.Linear(in_features, out_features)
-        self.value = nn.Linear(in_features, out_features)
-        self.MultiheadedAttention = nn.MultiheadAttention(embed_dim, num_heads)
-        
+        self.query = nn.Linear(embed_dim, kqv_dims)
+        self.key = nn.Linear(in_features, kqv_dims)
+        self.value = nn.Linear(in_features, kqv_dims)
+        self.MultiheadedAttention = nn.MultiheadAttention(kqv_dims, num_heads)
+
 
     def forward(self, input1, input2):
-        
+        ### calculate k,q,v
+        q = self.query()
+        k = self.key()
+        v = self.value()
+
+        ### calculate attention out
+        attn_out = self.MultiheadedAttention(q,k,v)
+
+        #calculate residual 
+        residual = input1 + attn_out
