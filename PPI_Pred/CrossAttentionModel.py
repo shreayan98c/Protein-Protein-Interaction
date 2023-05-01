f3fbc6a81953861e437358bc8bab921271156342
Nicholas Bowen <nbowen3@jhu.edu>
2023-05-01 17:25:40 -0400

    added potential pre-attention block

--- PPI_Pred/CrossAttentionModel.py
+++ PPI_Pred/CrossAttentionModel.py
@@ -3,8 +3,25 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class CrossAttentionModel(nn.Module):
-    def __init__(self, hidden_layers: list = None, dropout: float = 0.3):
+    def __init__(self, d: int 1):
         super().__init__()
+
+        # blocks of convolutional layers followed by batch normalization, relu, and max pooling
+        block_input1 = nn.Sequential(
+            nn.Conv1d(in_channels=d, out_channels=16, kernel_size=3, padding=1),
+            nn.BatchNorm1d(num_features=16),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm1d(num_features=32),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Conv1d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm1d(num_features=64),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2, stride=2),
+            nn.Linear(in_features=64 * 625, out_features=128)
+        )
 
     def forward(self, input1, input2):
         raise NotImplementedError
