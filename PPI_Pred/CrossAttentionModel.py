d6e86871c49b767236bcec47a174a849e045401a
Nicholas Bowen <nbowen3@jhu.edu>
2023-05-01 16:59:24 -0400

    empty class for cross attention

---
+++ PPI_Pred/CrossAttentionModel.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleLinearModel(nn.Module):
+    def __init__(self, hidden_layers: list = None, dropout: float = 0.3):
+        super().__init__()
+
+    def forward(self, input1, input2):
+        raise NotImplementedError
