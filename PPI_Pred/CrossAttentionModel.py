f033e4090c568b11e8859fe885ab596fdd8dbbe0
jacky <jc200108@gmail.com>
2023-05-02 15:22:30 -0400

    correct ff placement

--- PPI_Pred/CrossAttentionModel.py
+++ PPI_Pred/CrossAttentionModel.py
@@ -2,26 +2,24 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class SublayerConnection(nn.Module):
-    """
-    A residual connection followed by a layer norm.
-    Note for code simplicity the norm is first as opposed to last.
-    """
 
-    def __init__(self, size, dropout):
-        super(SublayerConnection, self).__init__()
-        self.norm = nn.LayerNorm(size)
+class PositionwiseFeedForward(nn.Module):
+    "Implements FFN equation, from the Annotated Transformer."
+
+    def __init__(self, d_model, d_ff, dropout=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x, sublayer):
-        "Apply residual connection to any sublayer with the same size."
-        return self.norm(x + self.dropout(sublayer(x)))
+    def forward(self, x):
+        return self.w_2(self.dropout(self.w_1(x).relu()))
 
 class CrossAttentionBlock(nn.Module):
     """
     Cross Attention Block with res connections, batch norm and a feed forward nn as defined in Attention is All You Need"
     """
-    def __init__(self,embed_dim, kqv_dims, num_heads, dropout=0.0, bias=True, add_bias_kv=False, add_zero_attn=False, kdim=None,\
+    def __init__(self,embed_dim, kqv_dims, num_heads, d_ff, dropout=0.0, bias=True, add_bias_kv=False, add_zero_attn=False, kdim=None,\
         vdim=None, batch_first=False, device=None, dtype=None):
         """ 
         Instantiation takes same args as torch.nn.MultiheadedAttention
@@ -32,19 +30,23 @@ class CrossAttentionBlock(nn.Module):
    
         self.MultiheadedAttention = nn.MultiheadAttention(kqv_dims * num_heads)
         self.l_norm = nn.LayerNorm(embed_dim)
-        self.dropout = nn.Dropout(dropout)
+        
+        self.ff = PositionwiseFeedForward(embed_dim, d_ff)
 
 
     def forward(self, input1, input2):
-        ### calculate k,q,v
-        q = self.query(input1)
-        k = self.key()
-        v = self.value()
 
-        ### calculate attention out
-        attn_out = self.MultiheadedAttention(q,k,v)
+        ### calculate attention out + residual connection and layer norm
+        attn_out = self.MultiheadedAttention(input1,input2,input2)
+        attn_out = self.l_norm(input1 + attn_out)
 
-        #calculate residual 
-        residual = input1 + attn_out
+        ### FF net followed by add and layer norm
+        ff_out = self.ff(attn_out)
+        ff_out = self.l_norm(ff_out + attn_out)
+        return ff_out
+
+
+
+
 
 
