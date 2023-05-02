5e72ff744eece6ab3b9147e37b30de1daf028f41
Alexandra Szewc <alexandra.szewc@gmail.com>
2023-05-02 15:19:11 -0400

    update arguments and masking

--- PPI_Pred/self_attention.py
+++ PPI_Pred/self_attention.py
@@ -21,10 +21,11 @@ class SDP_Attention(nn.Module):
         self.mask = mask
         self.dropout = dropout
 
-    def forward(self, sequence):
+    def forward(self):
         score = torch.matmul(self.query, self.key.transpose(-2, -1)) / torch.sqrt(self.dim)
 
-        # Optional Masking
+        # Optional Masking (masks indicated values to -inf)
+        # Fills elements of self tensor with value where mask is True. (-pytorch)
         if self.mask is not None:
             score = score.masked_fill(self.mask.view(score.size()), -float(np.inf))
 
