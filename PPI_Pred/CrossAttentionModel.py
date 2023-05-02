fe9f6fc89e60a1be2daa0bf8a8b2355501685983
jacky <jc200108@gmail.com>
2023-05-02 15:52:08 -0400

    correct multi headed attention inout

--- PPI_Pred/CrossAttentionModel.py
+++ PPI_Pred/CrossAttentionModel.py
@@ -19,25 +19,39 @@ class CrossAttentionBlock(nn.Module):
     """
     Cross Attention Block with res connections, batch norm and a feed forward nn as defined in Attention is All You Need"
     """
-    def __init__(self,embed_dim, kqv_dims, num_heads, d_ff, dropout=0.0, bias=True, add_bias_kv=False, add_zero_attn=False, kdim=None,\
+    def __init__(self,embed_dim, num_heads, ff_dim, dropout=0.0, bias=True, add_bias_kv=False, add_zero_attn=False, kdim=None,\
         vdim=None, batch_first=False, device=None, dtype=None):
         """ 
         Instantiation takes same args as torch.nn.MultiheadedAttention
+        Args:
+            embed_dims = 
+            num_heads = the number of heads
+            dd_dim = dimensions of the compressed forward neural net layer. 
         """
         super().__init__()
 
         # Block to pass input 1 through before passing to cross attention layer
-   
-        self.MultiheadedAttention = nn.MultiheadAttention(kqv_dims * num_heads)
+        self.MultiheadedAttention = nn.MultiheadAttention(embed_dim,num_heads, bias,add_bias_kv,add_zero_attn,kdim,vdim,batch_first,device,dtype)
         self.l_norm = nn.LayerNorm(embed_dim)
-        
-        self.ff = PositionwiseFeedForward(embed_dim, d_ff)
+
+        self.q_w = nn.Linear(embed_dim, embed_dim)
+        self.k_w = nn.Linear(embed_dim, embed_dim)
+        self.v_w = nn.Linear(embed_dim, embed_dim)
+    
+        # feed forward neural net
+        self.ff = PositionwiseFeedForward(embed_dim, ff_dim)
 
 
     def forward(self, input1, input2):
 
+        ### calculate query key value
+        query = self.q_w(input1)
+        key = self.q_w(input2)
+        value = self.q_w(value)
+
+
         ### calculate attention out + residual connection and layer norm
-        attn_out = self.MultiheadedAttention(input1,input2,input2)
+        attn_out = self.MultiheadedAttention(query,key,value)
         attn_out = self.l_norm(input1 + attn_out)
 
         ### FF net followed by add and layer norm
