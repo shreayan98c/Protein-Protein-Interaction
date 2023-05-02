ba7d8bf6db62fd7c496e4d16487daa1f7da537c8
jacky <jc200108@gmail.com>
2023-05-02 14:27:04 -0400

    add sublayer connections

--- PPI_Pred/CrossAttentionModel.py
+++ PPI_Pred/CrossAttentionModel.py
@@ -2,6 +2,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+
+    def __init__(self, size, dropout):
+        super(SublayerConnection, self).__init__()
+        self.norm = nn.LayerNorm(size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        "Apply residual connection to any sublayer with the same size."
+        return self.norm(x + self.dropout(sublayer(x)))
+
 class CrossAttentionBlock(nn.Module):
     """
     Cross Attention Block with res connections, batch norm and a feed forward nn as defined in Attention is All You Need"
@@ -14,10 +29,10 @@ class CrossAttentionBlock(nn.Module):
         super().__init__()
 
         # Block to pass input 1 through before passing to cross attention layer
-        self.query = nn.Linear(embed_dim, kqv_dims)
-        self.key = nn.Linear(embed_dim, kqv_dims)
-        self.value = nn.Linear(embed_dim, kqv_dims)
-        self.MultiheadedAttention = nn.MultiheadAttention(kqv_dims, num_heads)
+   
+        self.MultiheadedAttention = nn.MultiheadAttention(kqv_dims * num_heads)
+        self.l_norm = nn.LayerNorm(embed_dim)
+        self.dropout = nn.Dropout(dropout)
 
 
     def forward(self, input1, input2):
@@ -31,3 +46,5 @@ class CrossAttentionBlock(nn.Module):
 
         #calculate residual 
         residual = input1 + attn_out
+
+
